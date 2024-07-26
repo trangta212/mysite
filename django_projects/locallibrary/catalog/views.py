09a3f9f292eb74ed4b295ccfb119a376e490dbd6
Trang <tathutrang212@gmail.com>
2024-07-26 14:38:15 +0700

    Django Tutorial Part 7: Sessions framework

--- django_projects/locallibrary/catalog/views.py
+++ django_projects/locallibrary/catalog/views.py
@@ -8,6 +8,9 @@ def index(request):
   num_instances = BookInstance.objects.all().count()
   num_instances_available = BookInstance.objects.filter(status__exact='a').count()
   num_authors = Author.objects.count()
+    # Number of visits to this view, as counted in the session variable.
+  num_visits = request.session.get('num_visits', 1)
+  request.session['num_visits'] = num_visits + 1
   context = {
     'num_books': num_books,
     'num_instances': num_instances,
